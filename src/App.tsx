8bdbd8d9ebb663444c7f73f721c92d2bbba98e53
gentrithalili <gentrithalili11@gmail.com>
2022-03-07 22:04:00 +0100

    Add basic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./components/Button/Button";
+import { useConfirmation } from "./context/ConfirmationContext/ConfirmationContext";
 
 function App() {
+  const [text, setText] = useState("Waiting confirmation ...");
+  const { confirm } = useConfirmation();
+
+  const handleClick = async () => {
+    try {
+      await confirm({
+        text: "Are you sure?",
+        description: "Lorem ipsum dolor sit.",
+      });
+
+      setText("Confirmed!");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App text-center">
-      <h1>Custom promise-based confirmation dialogs</h1>
+      <div className="mt">
+        <h1 className="mb">{text}</h1>
+        <Button onClick={handleClick}>confirm</Button>
+      </div>
     </div>
   );
 }
